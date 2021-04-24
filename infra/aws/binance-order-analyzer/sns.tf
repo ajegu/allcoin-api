9cb04ab14dffad0681a9cce58cc2be7f4f790492
Allan JEGU <allanjegu@gmail.com>
2021-04-24 21:03:15 +0200

    ALC-44 Deploy lamba order buy

--- infra/aws/binance-order-analyzer/sns.tf
+++ infra/aws/binance-order-analyzer/sns.tf
@@ -1,3 +1,7 @@
 resource "aws_sns_topic" "binance_order_analyzer" {
     name = "${var.app_name}-${var.lambda_name}Topic"
 }
+
+output "AWS_SNS_TOPIC_ORDER_ANALYZER_ARN" {
+    value = aws_sns_topic.binance_order_analyzer.arn
+}
